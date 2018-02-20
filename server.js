f5668f6b835d0ecf01809105d109a559c0426c09
Glitch (mesquite-novel) <none>
2018-02-20 12:17:09 +0000

    🚍👔 Checkpoint server.js:692490/212

--- server.js
+++ server.js
@@ -3,16 +3,25 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 
+const mongoUrl = process.env.MONGOLAB_URI;
+
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use(express.static(process.cwd() + '/views'));
 
-app.get('/new/:url', (req, res) => {
-  res.json({
+function handleUrl(url) {
+  return {
     original_url: '',
     short_url: ''
-  });
+  }
+}
+
+app.get('/new', (req, res) => {
+  res.send("Error: You need to add a proper url");
+});
+app.get('/new/:url', (req, res) => {
+  res.json(handleUrl(req.params.query));
 });
 
 app.listen(process.env.PORT, () => {
