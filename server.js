5e14742138fd8f89ceaff95e1dbccfc36c04b21e
Glitch (mesquite-novel) <none>
2018-02-20 12:38:15 +0000

    🚎🍐 Checkpoint server.js:692490/148

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.use(bodyParser.json());
 
 app.use(express.static(process.cwd() + '/views'));
 
+function validateURL(url) {
+    const regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+    return regex.test(url);
+}
+
 function handleUrl(url) {
   return {
     original_url: '',
@@ -29,5 +34,8 @@ app.listen(process.env.PORT, () => {
 });
 
 const mongoUrl = process.env.MONGOLAB_URI;
-mongo.connect(
-
+mongo.connect(mongoUrl, (err, db) => {
+  if (err) throw err;
+  const urls = db.collection('shortened_urls');
+  console.log('Connected to MongoDB');
+});
