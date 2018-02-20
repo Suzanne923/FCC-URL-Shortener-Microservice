9a5c17e3ac68fdcfba9c7d384be2cf42f55478b4
Glitch (mesquite-novel) <none>
2018-02-20 15:35:28 +0000

    project finished

--- server.js
+++ server.js
@@ -15,11 +15,13 @@ app.listen(process.env.PORT, () => {
   console.log('Node.js listening ...');
 });
 
+// Checks if the url is a valid url
 function validateUrl(url) {
     const regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
     return regex.test(url);
 }
 
+// Handles the url request and returns json
 function handleUrl(url) {  
   const shortUrl = 'https://mesquite-novel.glitch.me/' + shortid.generate();
   storeUrl(url, shortUrl);
@@ -29,6 +31,7 @@ function handleUrl(url) {
   };  
 }
 
+// Stores a new url in the database
 function storeUrl(url, shortUrl) {
   mongo.connect(mongoUrl, (err, client) => {
     if (err) {
@@ -54,6 +57,7 @@ function storeUrl(url, shortUrl) {
   });
 }
 
+// Fetches the url from database and redirects user
 const fetchUrl = (req, res) => {
   const url = "https://mesquite-novel.glitch.me/" + req.params.url;
   mongo.connect(mongoUrl, (err, client) => {
@@ -70,15 +74,17 @@ const fetchUrl = (req, res) => {
         }
         if (data) { 
           res.redirect(data.url);
+        } else {
+          res.json({error: "Url not found."});
         };
       });
     }
   });
 }
 
-// routes
+// Routes
 app.get('/new', (req, res) => {
-  res.send("Error: You need to add a proper url");
+  res.send("error: You need to add a proper url");
 });
 app.get('/new/:url(*)', (req, res) => {
   const url = req.params.url;
