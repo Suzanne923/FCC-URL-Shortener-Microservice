d1650d721c764d43609ba7bf0301953544a21984
Glitch (mesquite-novel) <none>
2018-02-20 12:48:48 +0000

    🎁⛱ Checkpoint server.js:692490/249

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 app.use(express.static(process.cwd() + '/views'));
 
-function validateURL(url) {
+function validateUrl(url) {
     const regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
     return regex.test(url);
 }
@@ -26,7 +26,12 @@ app.get('/new', (req, res) => {
   res.send("Error: You need to add a proper url");
 });
 app.get('/new/:url', (req, res) => {
-  res.json(handleUrl(req.params.query));
+  const url = req.params.query;
+  if (validateUrl(url)) {
+    res.json(handleUrl(req.params.query));
+  } else {
+    res.json({error: "Incorrect url format"});
+  }
 });
 
 app.listen(process.env.PORT, () => {
@@ -36,6 +41,6 @@ app.listen(process.env.PORT, () => {
 const mongoUrl = process.env.MONGOLAB_URI;
 mongo.connect(mongoUrl, (err, db) => {
   if (err) throw err;
-  const urls = db.collection('shortened_urls');
+  //let urls = db.getCollection('shortened_urls');
   console.log('Connected to MongoDB');
 });
