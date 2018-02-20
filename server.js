6c0ad9d59e62372ad142b71ce95d92a341f9ff46
Glitch (mesquite-novel) <none>
2018-02-20 12:06:36 +0000

    🌞🌎 Checkpoint server.js:692490/117 README.md:692490/2 .env:692490/1

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ app.use(bodyParser.json());
 
 app.use(express.static(process.cwd() + '/views'));
 
+app.get('/new/:url', (req, res) => {
+  res.json({
+    original_url: '',
+    short_url: ''
+  });
+});
+
 app.listen(process.env.PORT, () => {
   console.log('Node.js listening ...');
 });
